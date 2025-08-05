c39b14140124e93bb55f4250f4c4d6a9ea79ef7c
Igor <72355380+YukiKras@users.noreply.github.com>
2025-08-06 05:44:13 +0700

    изменил порт в скрипте

--- torrserver-port-change-to-8080.sh
+++ torrserver-port-change-to-8080.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sed -i 's/8090/8080/g' /etc/systemd/system/torrserver.service
+sed -i 's/8090/80/g' /etc/systemd/system/torrserver.service
 systemctl daemon-reload
 systemctl restart torrserver
 IPv4=$(python3 -c "print('$(ip a | grep /32)'.split('inet ')[1].split('/32')[0])")
-echo "torrserver перенастроен на порт 8080: https://$IPv4:8080"
+echo "torrserver перенастроен на порт 80: https://$IPv4:80"
 echo "Данные для подключения не изменились "
-sed -i 's/8090/8080/g' /root/TorrServer.log
+sed -i 's/8090/80/g' /root/TorrServer.log
